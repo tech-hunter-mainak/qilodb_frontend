c9ecd09c416a1681feb771d400ee1b27970e308d
tech-hunter-mainak <153637016+tech-hunter-mainak@users.noreply.github.com>
2025-04-12 17:52:11 +0530

    lint fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import About from './pages/About'
@@ -6,7 +6,6 @@ import Contect from './pages/Contect'
 import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <React.StrictMode>
