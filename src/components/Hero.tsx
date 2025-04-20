9e4b114c72d9776f921c2aa58d81a0fe8b4028f2
tech-hunter-mainak <153637016+tech-hunter-mainak@users.noreply.github.com>
2025-04-20 15:19:27 +0530

    images fixed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,11 +8,11 @@ function Hero() {
                             <div className="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
                                 <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl flex flex-col">
                                     <div className="block xl:inline">QiloDB</div>
-                                    <div className="block text-indigo-600 xl:inline">Help You Build Faster.</div>
+                                    <div className="block text-blue-600 xl:inline">Help You Build Faster.</div>
                                 </h1>
                                 <p className="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">It's never been easier to build beautiful websites that convey your message and tell your story.</p>
                                 <div className="relative flex flex-col sm:flex-row sm:space-x-4">
-                                    <a href="#_" className="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto">
+                                    <a href="#_" className="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-blue-600 rounded-md sm:mb-0 hover:bg-blue-700 sm:w-auto">
                                         Try It Free
                                         <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
                                     </a>
@@ -23,8 +23,7 @@ function Hero() {
                             </div>
                         </div>
                         <div className="w-full md:w-1/2">
-                            <div className="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl">
-                                <img src="https://images.unsplash.com/photo-1498049860654-af1a5c566876?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" />
+                            <div className="w-full h-80 overflow-hidden rounded-md shadow-xl sm:rounded-xl bg-[url('./image_qilodb_front.jpg')] bg-cover bg-center">
                             </div>
                         </div>
                     </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,7 +10,7 @@ function NavBar() {
                 <div className="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
                     <div className="relative flex flex-col md:flex-row">
                         <a href="#_" className="flex items-center mb-5 font-medium text-gray-900 lg:w-auto lg:items-center lg:justify-center md:mb-0">
-                            <span className="mx-auto text-xl font-black leading-none text-gray-900 select-none">AI<span className="text-indigo-600">.</span></span>
+                            <img src="favicon.ico" alt="" className='w-28 -m-4' />
                         </a>
                         <nav className="flex flex-wrap items-center mb-5 text-base md:mb-0 md:pl-8 md:ml-8 md:border-l md:border-gray-200">
                             <button className="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900" onClick={()=>navigate('/')}>Home</button>
@@ -24,7 +24,7 @@ function NavBar() {
                         <a href="/" className="text-base font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900">
                             Sign in
                         </a>
-                        <a href="/" className="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600">
+                        <a href="/" className="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-blue-500 border border-transparent rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600">
                             Sign up
                         </a>
                     </div>
